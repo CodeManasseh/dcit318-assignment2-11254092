5053a781e8581d085b4b3b41075f1cfaf5b4c121
ABBEY EMMANUEL NII ABBEY <maquaye009@st.ug.edu.gh>
2025-08-06 21:18:19 +0000

    Task 2

--- AbstractClassesApp/Program.cs
+++ AbstractClassesApp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public abstract class Shape
+{
+    public abstract double GetArea();
+}
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+public class Rectangle : Shape
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+    public Rectangle(double length, double width)
+    {
+        Length = length;
+        Width = width;
+    }
+    public override double GetArea()
+    {
+        return Length * Width;
+    }
+}
+// Abstract Classes and Methods
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Circle area: {circle.GetArea()}");
+            Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        }
+    }
