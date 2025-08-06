a25b474dad69dca112921009503780750c463a51
ABBEY EMMANUEL NII ABBEY <maquaye009@st.ug.edu.gh>
2025-08-06 21:11:47 +0000

    Task 1

--- InheritanceApp/Program.cs
+++ InheritanceApp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal generic = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            generic.MakeSound();  // prints "Some generic sound"
+            dog.MakeSound();      // prints "Bark"
+            cat.MakeSound();      // prints "Meow"
+        }
+    }
